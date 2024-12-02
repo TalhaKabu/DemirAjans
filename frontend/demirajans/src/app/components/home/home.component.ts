1921b1f863af5fbd933cb7920c68ecc88e5fa201
talha kabu <kabuvetalha@gmail.com>
2024-12-02 17:11:21 +0300

    category resimleri ile beraber bitirildi

--- frontend/demirajans/src/app/components/home/home.component.ts
+++ frontend/demirajans/src/app/components/home/home.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CategoryDto } from '../../services/categories/models';
+import { CategoryService } from '../../services/categories/category.service';
 
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  //#region Props
+  categoryList!: CategoryDto[];
+  //#endregion
 
+  //#region Utils
+  getCategoryListByAppearInFront() {
+    this.categoryService.getListByAppearInFront(true).subscribe({
+      next: (n) => (this.categoryList = n),
+      error: (e) => console.log(e),
+      complete: () => console.log(this.categoryList),
+    });
+  }
+  //#endregion
+
+  //#region Ctor
+  constructor(private categoryService: CategoryService) {}
+  //#endregion
+
+  //#region Methods
+  ngOnInit() {
+    this.getCategoryListByAppearInFront();
+  }
+  //#endregion
 }
--- frontend/demirajans/src/app/components/shared/navbar/navbar.component.ts
+++ frontend/demirajans/src/app/components/shared/navbar/navbar.component.ts
@@ -1,7 +1,4 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { CategoryService } from '../../../services/categories/category.service';
-import { catchError, Observable, tap } from 'rxjs';
-import { CategoryDto } from '../../../services/categories/models';
 
 @Component({
   selector: 'app-navbar',
@@ -14,27 +11,16 @@ export class NavbarComponent implements OnInit {
   //#region Props
   firstToolbarRowMobile: boolean = false;
   innerWidth: any;
-
-  categoryList!: CategoryDto[];
   //#endregion
 
   //#region Utils
-  getCategoryListByAppearInFront() {
-    this.categoryService.getListByAppearInFront(true).subscribe({
-      next: (n) => (this.categoryList = n),
-      error: (e) => console.log(e),
-      complete: () => console.log(this.categoryList),
-    });
-  }
   //#endregion
 
   //#region Ctor
-  constructor(private categoryService: CategoryService) {}
   //#endregion
 
   //#region Methods
   ngOnInit() {
-    this.getCategoryListByAppearInFront();
     this.innerWidth = window.innerWidth;
     if (this.innerWidth < 768) this.firstToolbarRowMobile = true;
     else this.firstToolbarRowMobile = false;
