59c061c9fa8250f644c3467a2287870b08c5a213
talha kabu <kabuvetalha@gmail.com>
2024-12-05 16:56:00 +0300

    giris tamamlandi

--- frontend/demirajans/src/app/components/login/login.component.ts
+++ frontend/demirajans/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { LoginDto } from '../../services/auth/models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,21 +11,25 @@ import { LoginDto } from '../../services/auth/models';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  loginDto: LoginDto = <LoginDto>{};
 
-  ngOnInit(): void {
-    // this.authService
-    //   .login(<LoginDto>{
-    //     username: 'admin',
-    //     password: 'admin1234',
-    //   })
-    //   .subscribe({
-    //     next: (n) => (
-    //       console.log(n),
-    //       localStorage.setItem('token', n.token),
-    //       localStorage.setItem('expiration', n.expiration.toUTCString())
-    //     ),
-    //     error: (e) => console.log(e),
-    //   });
+  login() {
+    this.authService
+      .login(<LoginDto>{
+        username: 'admin',
+        password: 'admin1234',
+      })
+      .subscribe({
+        next: (n) => (
+          localStorage.setItem('token', n.token),
+          localStorage.setItem('expiration', n.expiration.toString()),
+          this.router.navigate([''])
+        ),
+        error: (e) => console.log(e),
+      });
   }
+
+  constructor(private authService: AuthService, protected router: Router) {}
+
+  ngOnInit(): void {}
 }
