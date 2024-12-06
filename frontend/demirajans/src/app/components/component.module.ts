0518b3f0e1a261c687b1582e8313a7a88c3b0662
talha kabu <kabuvetalha@gmail.com>
2024-12-07 02:26:48 +0300

    sonunda yeni navbar

--- frontend/demirajans/src/app/components/component.module.ts
+++ frontend/demirajans/src/app/components/component.module.ts
@@ -2,14 +2,7 @@ import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ComponentRoutingModule } from './component-routing.module';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -17,23 +10,12 @@ import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    SidebarComponent,
     NavbarComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent,
   ],
-  imports: [
-    ComponentRoutingModule,
-    CommonModule,
-    FormsModule,
-    MatListModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatDividerModule,
-  ],
-  exports: [SidebarComponent, NavbarComponent],
+  imports: [ComponentRoutingModule, CommonModule, FormsModule],
+  exports: [NavbarComponent],
 })
 export class ComponentModule {}
