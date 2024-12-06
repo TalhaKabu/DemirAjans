639ff12651598b28dff85b48bbcba12a2cd5d641
talha kabu <kabuvetalha@gmail.com>
2024-12-06 18:02:02 +0300

    kullanici adi ve sifre bos kontrolu tamamlandi

--- frontend/demirajans/src/app/components/login/login.component.ts
+++ frontend/demirajans/src/app/components/login/login.component.ts
@@ -15,17 +15,29 @@ export class LoginComponent implements OnInit {
   loginErrMsg: string = '';
 
   login() {
-    this.authService.login(this.loginDto).subscribe({
-      next: (n) => (
-        localStorage.setItem('token', n.token), this.router.navigate([''])
-      ),
-      error: (e) => {
-        console.log(e);
-        if (e.status === 401) {
-          this.loginErrMsg = 'Kullanıcı adı veya şifre yanlış.';
-        }
-      },
-    });
+    if (
+      this.loginDto.username === '' ||
+      this.loginDto.password === '' ||
+      this.loginDto.username === undefined ||
+      this.loginDto.password === undefined ||
+      this.loginDto.username === null ||
+      this.loginDto.password === null
+    ) {
+      this.loginErrMsg = 'Kullanıcı adı ve şifre boş olamaz.';
+      return;
+    } else {
+      this.authService.login(this.loginDto).subscribe({
+        next: (n) => (
+          localStorage.setItem('token', n.token), this.router.navigate([''])
+        ),
+        error: (e) => {
+          console.log(e);
+          if (e.status === 401) {
+            this.loginErrMsg = 'Kullanıcı adı veya şifre yanlış.';
+          }
+        },
+      });
+    }
   }
 
   constructor(private authService: AuthService, private router: Router) {}
