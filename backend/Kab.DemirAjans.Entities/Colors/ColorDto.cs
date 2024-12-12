4c7fd36aea3d486b0cc637e74e38651f5ed1365c
talha kabu <kabuvetalha@gmail.com>
2024-12-13 00:50:31 +0300

    product kismi tamamlandi

--- backend/Kab.DemirAjans.Entities/Colors/ColorDto.cs
+++ backend/Kab.DemirAjans.Entities/Colors/ColorDto.cs
@@ -4,6 +4,7 @@ namespace Kab.DemirAjans.Entities.Colors;
 
 public class ColorDto : AggregateRoot
 {
+    public int Id { get; set; }
     public string? Name { get; set; }
     public string Code { get; set; }
     public string Header { get; set; }
--- frontend/demirajans/src/app/components/product/product.component.ts
+++ frontend/demirajans/src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ColorDto, ProductDto } from '../../services/products/models';
 import { environment } from '../../../environments/environment';
@@ -17,8 +17,12 @@ export class ProductComponent implements OnInit {
   productId!: number;
   product!: ProductDto;
   selectedColor!: ColorDto;
+  images: string[] = [];
+  selectedImage!: string;
   apiUrl!: string;
   categoryName!: string;
+
+  @ViewChild('thumbnails') thumbnails!: ElementRef<HTMLImageElement>;
   //#endregion
 
   //#region Utils
@@ -27,6 +31,8 @@ export class ProductComponent implements OnInit {
       next: (n) => (
         (this.product = n),
         ((this.selectedColor = this.product.colors[0]),
+        this.images.push(this.product.imageName, this.selectedColor.imageName),
+        (this.selectedImage = this.images[1]),
         console.log(this.product))
       ),
       error: (e) => console.log(e),
@@ -52,6 +58,40 @@ export class ProductComponent implements OnInit {
     this.activatedRoute.queryParamMap.subscribe((params) => {
       this.categoryName = params.get('categoryDto') + '';
     });
+  }
+
+  activeImage(img: string): boolean {
+    return this.selectedImage === img;
+  }
+
+  arrowOnClick(value: boolean, imageName: string) {
+    if (value) {
+      var index = this.images.indexOf(imageName);
+      if (index === this.images.length - 1) {
+        this.selectedImage = this.images[0];
+      } else {
+        this.selectedImage = this.images[index + 1];
+      }
+    } else {
+      var index = this.images.indexOf(imageName);
+      if (index === 0) {
+        this.selectedImage = this.images[this.images.length - 1];
+      } else {
+        this.selectedImage = this.images[index - 1];
+      }
+    }
+  }
+
+  imgOnClick(img: string) {
+    this.selectedImage = img;
+  }
+
+  colorOnClick(colorId: number) {
+    this.selectedColor = this.product.colors.find((x) => x.id === colorId)!;
+    var imgs = this.images.filter((x) => x === this.product.imageName);
+    imgs.push(this.selectedColor.imageName);
+    this.images = imgs;
+    this.selectedImage = this.selectedColor.imageName;
   }
   //#endregion
 }
