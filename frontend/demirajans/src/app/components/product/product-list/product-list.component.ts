5d3c8da47d3ca0dfe4cde2f71c002e47120580f1
talha kabu <kabuvetalha@gmail.com>
2024-12-12 23:15:34 +0300

    product list to product yonlendirme yapildi

--- frontend/demirajans/src/app/components/product/product-list/product-list.component.ts
+++ frontend/demirajans/src/app/components/product/product-list/product-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../../services/products/product.service';
 import { ProductDto } from '../../../services/products/models';
 import { environment } from '../../../../environments/environment';
+import { CategoryDto } from '../../../services/categories/models';
 
 @Component({
   selector: 'app-product-list',
@@ -18,6 +19,7 @@ export class ProductListComponent implements OnInit {
   apiUrl!: string;
 
   productList!: ProductDto[];
+  @Input() categoryList!: CategoryDto[];
   //#endregion
 
   //#region Utils
@@ -44,6 +46,7 @@ export class ProductListComponent implements OnInit {
   //#region Ctor
   constructor(
     private activatedRoute: ActivatedRoute,
+    private router: Router,
     private productService: ProductService
   ) {}
   //#endregion
@@ -57,6 +60,17 @@ export class ProductListComponent implements OnInit {
         params['sid'] !== undefined ? parseInt(params['sid']) : 0;
       if (this.selectedCategoryId === 0) this.getProductListByAppearInFront();
       else this.getListByCategoryIdAndSubCategoryId();
+    });
+  }
+
+  productOnClick(productId: number) {
+    this.router.navigate(['product', productId], {
+      queryParams: {
+        categoryDto: this.categoryList.find(
+          (x) =>
+            x.id == this.productList.find((x) => x.id === productId)!.categoryId
+        )!.name,
+      },
     });
   }
   //#endregion
