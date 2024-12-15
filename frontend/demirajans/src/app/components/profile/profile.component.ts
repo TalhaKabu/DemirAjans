a5fc46ddc6033605623152b24af26cca8433a935
talha kabu <kabuvetalha@gmail.com>
2024-12-15 15:44:09 +0300

    profile baslandi

--- frontend/demirajans/src/app/components/profile/profile.component.ts
+++ frontend/demirajans/src/app/components/profile/profile.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   standalone: false,
-  
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
-})
-export class ProfileComponent {
 
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss',
+})
+export class ProfileComponent implements OnInit {
+  passwordVisible: boolean = false;
+
+  ngOnInit(): void {}
 }
