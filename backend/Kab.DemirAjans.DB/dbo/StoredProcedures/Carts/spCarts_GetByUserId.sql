0046e9d600dcd00976253f68e669bbc116a66ff1
talha kabu <kabuvetalha@gmail.com>
2024-12-16 12:51:32 +0300

    carts sp created

---
+++ backend/Kab.DemirAjans.DB/dbo/StoredProcedures/Carts/spCarts_GetByUserId.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spCarts_GetByUserId]
+	@UserId int
+AS
+BEGIN
+	SELECT
+		 Id
+		,UserId
+		,ProductId
+		,Quantity
+		,CreationDate
+		,LastModificationDate
+	FROM dbo.[Carts]
+	WHERE UserId = @UserId;
+END
---
+++ backend/Kab.DemirAjans.DB/dbo/StoredProcedures/Carts/spCarts_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spCarts_Insert]
+	@UserId int,
+	@ProductId int,
+	@Quantity int,
+	@CreationDate datetime2(7),
+	@LastModificationDate datetime2(7)
+AS
+BEGIN
+	INSERT INTO dbo.[Carts] (UserId, ProductId, Quantity, CreationDate, LastModificationDate)
+	VALUES (@UserId, @ProductId, @Quantity, @CreationDate, @LastModificationDate);
+END
+
